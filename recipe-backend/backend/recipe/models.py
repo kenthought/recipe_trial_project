9a9622d82add74f7c4c28880391c322ae888f027
kenthought <nananaupan@gmail.com>
2023-09-03 14:55:57 +0800

    altered table recipe - increased the max size of ingredients and instruction column

--- recipe-backend/backend/recipe/models.py
+++ recipe-backend/backend/recipe/models.py
@@ -7,8 +7,8 @@ class Recipe(models.Model):
     recipe = models.CharField(
          max_length=100, 
     )
-    ingredients = models.CharField(max_length=100)
-    instructions = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=1500)
+    instructions = models.CharField(max_length=1500)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name="recipe", on_delete=models.PROTECT

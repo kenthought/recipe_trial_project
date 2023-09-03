8fafa7c01e110034707c8fa3c2fb8a7b03b91d66
kenthought <nananaupan@gmail.com>
2023-09-03 14:57:37 +0800

    included is_superuser in user model

--- recipe-backend/backend/users/models.py
+++ recipe-backend/backend/users/models.py
@@ -38,10 +38,13 @@ class UserManager(BaseUserManager):
         **extra_fields
     ):
         extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff = True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser = True")
 
         return self.create_user(
             email,
@@ -59,8 +62,10 @@ class UserData(AbstractUser, PermissionsMixin):
     middle_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100)
     date_joined = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 

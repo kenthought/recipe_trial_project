53296ce81b583d702c6915dc63579db0847547ab
kenthought <nananaupan@gmail.com>
2023-09-05 16:14:51 +0800

    deleting comments

--- recipe-backend/backend/recipe/serializers.py
+++ recipe-backend/backend/recipe/serializers.py
@@ -6,16 +6,6 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['recipe', 'ingredients', 'instructions', 'user']
-
-    # def create(self, validated_data):
-    #     return self.Meta.model(**validated_data)
-
-    # def update(self, instance, validated_data):
-    #     instance.recipe = validated_data.get('recipe', instance.recipe)
-    #     instance.ingredients = validated_data.get('ingredients', instance.ingredients)
-    #     instance.instructions = validated_data.get('instructions', instance.instructions)
-    #     instance.user = validated_data.get('user', instance.user)
-    #     return instance
     
 class RecipeViewSerializer(serializers.ModelSerializer):
     user = UserSerializer()

13fd98be269884bfeacab2ca30de83f1dd32303f
kenthought <nananaupan@gmail.com>
2023-09-05 16:21:07 +0800

    added commments

--- recipe-frontend/middleware/auth.ts
+++ recipe-frontend/middleware/auth.ts
@@ -1,5 +1,6 @@
 import { authStore } from "~/stores/authStore";
 
+// Checkes whether the user is logged in. If not redirect to login page
 export default defineNuxtRouteMiddleware((to, from) => {
   const auth = authStore();
   if (!auth.isLoggedIn) return navigateTo("/auth/login", {replace: true});
--- recipe-frontend/middleware/user.ts
+++ recipe-frontend/middleware/user.ts
@@ -1,5 +1,6 @@
 import { authStore } from "~/stores/authStore";
 
+// Restrict logged in user from accessing login page
 export default defineNuxtRouteMiddleware((to, from) => {
   const auth = authStore();
   if (auth.isLoggedIn) return navigateTo("/", {replace: true});

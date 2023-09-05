7368d006709a7595fa16a142f5921209bf31e18e
kenthought <nananaupan@gmail.com>
2023-09-05 17:32:36 +0800

    added api documentation

--- recipe-backend/backend/backend/urls.py
+++ recipe-backend/backend/backend/urls.py
@@ -20,8 +20,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("api_schema", get_schema_view(title="Recipe API Schema", description="Guide for the Recipe REST API"), name="api_schema"),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'api_schema'}
+    ), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path("api/recipe/", include("recipe.urls")),
     path("api/users/", include("users.urls")),
